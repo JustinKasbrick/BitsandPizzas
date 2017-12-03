8a5a2d095b33bf61e26cbb72e01c46f4f7051bb9
Justin Kasbrick <justinkasbrick@Justins-MacBook-Pro.local>
2017-12-02 21:53:38 -0600

    React to action item clicks with the onOptionsItemSelected() method.

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -3,6 +3,7 @@ package com.hfad.bitsandpizzas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +18,16 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action var if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                // // TODO: 2017-12-02
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
