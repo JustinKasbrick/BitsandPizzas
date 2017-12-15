47f775a857fd9cca69196352fb1e5590514fff9b
JustinKasbrick <justink@iqmetrix.com>
2017-12-14 22:31:04 -0600

    Create an ActionBarDrawerToggle and disable/enable the share action item when the drawer opens/closes.

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +21,7 @@ public class MainActivity extends Activity {
     private String[] titles;
     private ListView drawerList;
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
 
@@ -74,6 +76,30 @@ public class MainActivity extends Activity {
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
         if(savedInstanceState == null)
             selectItem(0);
+        // Create the ActionBarToggle
+        drawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout, R.string.open_drawer, R.string.close_drawer) {
+            // Called when a drawer has settled in a completely closed state
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+            // Called when a drawer has settled in a completely opened state
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    // Called whenever we call invalidateOptionsMenu()
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the drawer is open, hide action items related to the content view
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
